333bc466b36397016e3af594de0cb90baa9b4ff0
Piyush Ahir <egpiyushahir@gmail.com>
2021-07-28 15:02:44 +0530

    Button Handlers

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var bill = document.querySelector(".bill-ipt");
-var btn = document.querySelector("#bill-btn");
+var billAmount = document.querySelector(".bill-ipt");
+var cashAmount = document.querySelector(".cash-ipt");
+var billButton = document.querySelector("#bill-btn");
+var cashDisplay = document.querySelector(".cash-div");
+var billButtonDisplay = document.querySelector(".bill-btn");
+var cashButton = document.querySelector(".cash-btn");
+var errDisplay = document.querySelector(".err-msg");
+var errMessage = document.querySelector("#err-opt");
 
-function clickHandler(){
-   var va = bill.value;
-   console.log("Input" + va);
+function billHandler(){
+   if(billAmount.value <= 0){
+      errMessage.innerHTML = "Please Enter a valid bill amount";
+      errDisplay.style.display = "block";
+   }else{
+      billButton.style.display = "none";
+      cashDisplay.style.display = "block";
+      errDisplay.style.display = "none";
+   }
 }
 
-btn.addEventListener("click", clickHandler);
+function cashHandler(){
+   
+}
+
+billButton.addEventListener("click", billHandler);
+cashButton.addEventListener("click", cashHandler);
