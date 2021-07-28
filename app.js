d3ce011250f0e0302b39945dd35e2bafc7f3ac0f
Piyush Ahir <egpiyushahir@gmail.com>
2021-07-28 16:00:11 +0530

    Notes calculation logic

--- app.js
+++ app.js
@@ -1,26 +1,71 @@
+const totalNotes = [2000,500,100,20,10,5,1];
+var retrnAmount = 0;
 var billAmount = document.querySelector(".bill-ipt");
 var cashAmount = document.querySelector(".cash-ipt");
 var billButton = document.querySelector("#bill-btn");
 var cashDisplay = document.querySelector(".cash-div");
+var billDisplay = document.querySelector(".bill-div");
 var billButtonDisplay = document.querySelector(".bill-btn");
 var cashButton = document.querySelector(".cash-btn");
 var errDisplay = document.querySelector(".err-msg");
 var errMessage = document.querySelector("#err-opt");
+var showNotes = document.querySelectorAll(".showNotes");
+var output = document.querySelector(".opt");
+
+function calNotes(){
+   let bill = billAmount.value;
+   let cash = cashAmount.value;
+   retrnAmount = cashAmount.value-billAmount.value;
+
+   if(retrnAmount == 0){
+      showErr("No need to return any amount");
+   }
+   else{
+      for(var i=0; i<totalNotes.length; i++){
+         var left = Math.floor(retrnAmount/totalNotes[i]);
+         if(left > 0){
+            showNotes[i].innerHTML = left;
+         }
+         retrnAmount -= (left*totalNotes[i]);
+      }
+
+      hideErr();
+      cashDisplay.style.display = "none";
+      billDisplay.style.display = "none";
+      output.style.display = "block";
+   }
+   
+}
 
 function billHandler(){
-   if(billAmount.value <= 0){
-      errMessage.innerHTML = "Please Enter a valid bill amount";
-      errDisplay.style.display = "block";
-   }else{
+   if(billAmount.value > 0){
       billButton.style.display = "none";
       cashDisplay.style.display = "block";
-      errDisplay.style.display = "none";
+      hideErr();
+   }else{
+      showErr("Please Enter a valid bill amount");
    }
 }
 
 function cashHandler(){
-   
+   if(cashAmount.value <= 0){
+      showErr("Enter a valid cash amount.")
+   }else if(Number(cashAmount.value) < Number(billAmount.value)){
+      showErr("Cash amount should be greater than bill amount.")
+   }else{
+      hideErr();
+      calNotes();
+   }
 }
 
 billButton.addEventListener("click", billHandler);
 cashButton.addEventListener("click", cashHandler);
+
+function hideErr(){
+   errDisplay.style.display = "none";
+}
+
+function showErr(msg){
+   errMessage.innerHTML = msg;
+   errDisplay.style.display = "block"; 
+}
